ba704d3730532b871fa08e0be207e1e82f19553c
Your Name <you@example.com>
2020-02-22 21:46:41 +0000

    Adde SerialPort

--- api-server/src/index.ts
+++ api-server/src/index.ts
@@ -1,6 +1,10 @@
 import express = require("express");
 import { validator } from './validator';
+import { SerialPort } from 'serialport'
 
+const portName = process.argv[2]
+
+const myPort = new SerialPort(portName, {baudRate: 9600});
 const PORT = process.env.PORT || 3000;
 const app = express();
 
@@ -9,3 +13,8 @@ app.use(validator);
 app.listen(PORT, () => {
     console.log("Server Started");
 });
+
+myPort.on('open', console.log);
+myPort.on('data', console.log);
+
+
