d562f933cb7dcd1b4942edc414484a24d26dc746
ImOverlord <ImOverlord@hotmail.com>
2020-02-22 23:20:24 +0100

    Added new Parser

--- api-server/src/index.ts
+++ api-server/src/index.ts
@@ -1,9 +1,13 @@
 import express = require("express");
 import { validator } from './validator';
-const SerialPort = require('serialport')
+const SerialPort = require('serialport');
+const Readline = SerialPort.parsers.Readline;
+const parser = new Readline()
+
 const portName = process.argv[2]
 
 const myPort = new SerialPort(portName, {baudRate: 9600});
+myPort.pipe(parser);
 const PORT = process.env.PORT || 3000;
 const app = express();
 
@@ -14,12 +18,18 @@ app.listen(PORT, () => {
 });
 
 myPort.on('open', () => {
-    
-	myPort.write('left\n');
+    myPort.write('l');
+    myPort.write('e');
+    myPort.write('f');
+    myPort.write('t');
 });
-myPort.on('data', (data: Buffer) => {
+
+parser.on('data', (data: Buffer) => {
     console.log(data.toString())
-	myPort.write('left\n');
+	myPort.write('l');
+    myPort.write('e');
+    myPort.write('f');
+    myPort.write('t');
 });
 
 
