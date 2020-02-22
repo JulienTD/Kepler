448cbc6ffe0cb8ce382210470503aaa2386614a8
ImOverlord <ImOverlord@hotmail.com>
2020-02-23 00:03:06 +0100

    Added Server

--- api-server/src/index.ts
+++ api-server/src/index.ts
@@ -1,4 +1,5 @@
 import express = require("express");
+import { Request, Response} from 'express';
 import { validator } from './validator';
 const SerialPort = require('serialport');
 const portName = process.argv[2]
@@ -6,19 +7,29 @@ const myPort = new SerialPort(portName, {baudRate: 9600});
 const PORT = process.env.PORT || 3000;
 const app = express();
 
+let ready = false;
+
 app.use(validator);
 
 app.listen(PORT, () => {
     console.log("Server Started");
 });
 
+app.post('/api/action/:type', (req: Request, res: Response) => {
+    const type = req.params.type;
+    console.log(`GOT ${type}`);
+    if (!ready)
+        return res.status(500).send("Not Ready");
+    myPort.write(`${type};`);
+    res.send("OK");
+})
+
 myPort.on('open', () => {
-    myPort.write("left;");
+    ready = true;
 });
 
 myPort.on('data', (data: Buffer) => {
     console.log(data.toString())
-    myPort.write("left;");
 });
 
 
